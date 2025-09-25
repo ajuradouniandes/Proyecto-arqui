1f016a32386791f1db04e2f5b1c1286c626f174f
Saray Rivas <saraysahar.r@gmail.com>
2025-09-25 16:27:05 -0500

    arreglosssss

--- inventories/urls.py
+++ inventories/urls.py
@@ -14,5 +14,5 @@ router.register(r"movements", InventoryMovementViewSet, basename="inventorymovem
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),  # /api/products/ endpoints
-    path("health-check/", include(health_check.urls)),  # /health-check/ endpoints
+    path("health-check/", health_check),  # /health-check/ endpoints
 ]
