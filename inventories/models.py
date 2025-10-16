66be4aed61049cca13fc41208796937e530235eb
Saray Rivas <saraysahar.r@gmail.com>
2025-10-16 17:56:50 -0500

    solucion error

--- inventories/models.py
+++ inventories/models.py
@@ -15,15 +15,6 @@ class Warehouse(models.Model):
     location = models.CharField(max_length=100)
     creation_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
-    
-class WarehouseCreation(models.Model):
-    id_warehouse_creation = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    location = models.CharField(max_length=100)
-    creation_date = models.DateTimeField(auto_now_add=True)
-    update_date = models.DateTimeField(auto_now=True)
-    inventory = models.ManyToManyField(Product, through='inventories')
-    
 class Shelve(models.Model):
     id_shelve= models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
@@ -48,6 +39,15 @@ class InventoryMovement(models.Model):
     quantity = models.IntegerField()
     movement_date = models.DateTimeField(auto_now_add=True)
     notes = models.TextField(null=True, blank=True)
+
+
+class WarehouseCreation(models.Model):
+    id_warehouse_creation = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    update_date = models.DateTimeField(auto_now=True)
+    inventory = models.ManyToManyField(Inventory, through='WarehouseInventory')
     
 
 
