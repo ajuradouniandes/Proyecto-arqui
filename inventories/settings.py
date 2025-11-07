530433fe937a45f6410f6441d341ca4b10d7b4eb
Saray Rivas <saraysahar.r@gmail.com>
2025-11-06 19:54:45 -0500

    .

--- inventories/settings.py
+++ inventories/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'databaseinventory',
-        'USER': 'proyecto_arqui',
+        'USER': 'postgres',
         'PASSWORD': 'arqui2025',
         'HOST': 'databaseinventory.cvkooum6o8l7.us-east-1.rds.amazonaws.com',
         'PORT': 5432,
