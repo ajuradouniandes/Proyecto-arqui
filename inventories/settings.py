d944195e1743c6d30fe4e431e34be81c170a2461
Saray Rivas <saraysahar.r@gmail.com>
2025-11-06 19:47:27 -0500

    .

--- inventories/settings.py
+++ inventories/settings.py
@@ -158,7 +158,7 @@ CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://localhost:6379/1',  # Cambia esto a la IP de tu servidor Redis si es diferente
+        'LOCATION': 'redis://proyecto-cache-uhmcki.serverless.use1.cache.amazonaws.com:6379',  
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         },
