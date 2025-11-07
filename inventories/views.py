eb6ba4d9bb6ecc8eacb32bf2cb522a82561f6d33
Saray Rivas <saraysahar.r@gmail.com>
2025-11-06 23:56:29 -0500

    .

--- inventories/views.py
+++ inventories/views.py
@@ -163,6 +163,7 @@ class OrderCreationViewSet(viewsets.ModelViewSet):
         
         
                 order = serializer.save()
+                print('Pedido guardado en cache:', cache.get(cache_key))
                 #cache.delete(cache_key)
                 headers = self.get_success_headers(serializer.data)
                 return Response(serializer.data, status=201, headers=headers)
