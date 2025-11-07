7cad9185cf812b6e61dea5a79ac59029e28a1d22
Saray Rivas <saraysahar.r@gmail.com>
2025-11-06 21:06:51 -0500

    .

--- inventories/celery.py
+++ inventories/celery.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'myproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'inventories.settings')
 
-app = Celery('myproject')
+app = Celery('inventories')
 
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
